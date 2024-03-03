e01430ed7c730c78cdf9dafec8e75ece8472a2e9
canakboyraz <canakboyraz@gmail.com>
2024-03-03 11:56:05 +0300

    deneme

--- Fabrika/Program.cs
+++ Fabrika/Program.cs
@@ -1,5 +1,8 @@
 ﻿//2)Bir fabrikada sabit maaşla çalışan işçiler, aile durumlarına göre ek maaş almaktadır.Bekar olanlar sadece maaş, evli ve çocuğu olmayanlar % 3 ek maaş, çocuk sayısı 1 olanlar % 5 ek maaş, çocuk sayısı 2 olanlar % 10 ek maaş, çocuk sayısı 3 ve daha fazla olanlar ise % 15 kadar ek maaş almaktadır. Bu göre, kullanıcıdan işçinin maaşı ve çocuk sayısını isteyerek, gerekli hesaplamayı yapıp ekrana yazdıran program.
 
+
+/* Try - Catch
+
 Console.WriteLine("Lütfen maaşını giriniz: ");
 double maas = Convert.ToDouble(Console.ReadLine);
 
@@ -8,34 +11,103 @@ string personel = Console.ReadLine();
 
 if (personel == "e")
 {
-    Console.Write("Lütfen çocuk sayısını girin ");
-    int sayi = Convert.ToInt32(Console.ReadLine());
+    try
+    {
+        Console.Write("Lütfen çocuk sayısını girin ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
 
-    if (sayi == 0)
+        if (sayi == 0)
+        {
+            Console.WriteLine("Maaşınız: " + maas);
+        }
+        else if (sayi == 1)
+        {
+            Console.WriteLine("Maaşınız: " + (maas * 1.05));
+        }
+        else if (sayi == 2)
+        {
+            Console.WriteLine("Maaşınız: " + (maas * 1.10));
+        }
+        else if (sayi >= 3)
+        {
+            Console.WriteLine("Maaşınız: " + (maas * 1.20));
+        } 
+
+    }
+    catch(Exception ex)
+    {
+        {
+            Console.WriteLine("Hatalı işlem lütfen tekrar deneyin.");
+            Console.WriteLine(ex.Message);
+
+        }
+
+    }
+    
+}
+else
+{
     {
         Console.WriteLine("Maaşınız: " + maas);
     }
-    else if(sayi == 1)
+}
+*/
+
+tekrar:
+Console.WriteLine("Lütfen maaşını giriniz: ");
+double maas = 0;
+try
+{
+    maas = Convert.ToDouble(Console.ReadLine());
+}
+catch (FormatException ex)
+{
+    Console.WriteLine("Geçersiz bir maaş değeri girdiniz.");
+    Console.WriteLine("Hata: " + ex.Message);
+    goto tekrar;
+}
+
+Console.Write("Lütfen Evli için 'e' Bekar için 'b' basın ");
+string personel = Console.ReadLine();
+
+if (personel == "e")
+{
+    tekrar1:
+    try
     {
-        Console.WriteLine("Maaşınız: " + (maas * 1.05));
+        
+        Console.Write("Lütfen çocuk sayısını girin ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+
+        if (sayi == 0)
+        {
+            Console.WriteLine("Maaşınız: " + maas);
+        }
+        else if (sayi == 1)
+        {
+            Console.WriteLine("Maaşınız: " + (maas * 1.05));
+        }
+        else if (sayi == 2)
+        {
+            Console.WriteLine("Maaşınız: " + (maas * 1.10));
+        }
+        else if (sayi >= 3)
+        {
+            Console.WriteLine("Maaşınız: " + (maas * 1.20));
+        }
     }
-    else if (sayi == 2)
-    {
-        Console.WriteLine("Maaşınız: " + (maas * 1.10));
-    }
-    else if (sayi >= 3)
-    {
-        Console.WriteLine("Maaşınız: " + (maas * 1.20));
-    }
-    else
+    catch (Exception ex)
     {
         Console.WriteLine("Hatalı işlem lütfen tekrar deneyin.");
+        Console.WriteLine(ex.Message);
+        goto tekrar1;
     }
 }
 else
 {
     Console.WriteLine("Maaşınız: " + maas);
 }
+
 
 
 
--- ParaCevirici/Program.cs
+++ ParaCevirici/Program.cs
@@ -1,5 +1,8 @@
 ﻿//Klavyeden kullanıcı tarafından paraMiktari cinsinden para miktarını, yine kullanıcı tarafından girilen Euro ve Dolar kuruna göre paraMiktari'nin kaç Euro ve kaç Dolar olduğunu bulan programı yazınız.,
 
+
+/*
+ 
 Console.WriteLine("Lütfen TL para miktarını giriniz: ");
 double paraMiktari = Convert.ToDouble(Console.ReadLine());
 
@@ -29,3 +32,66 @@ else
     goto tekrar;
 }
 
+*/
+
+try
+{
+    Console.WriteLine("Lütfen TL para miktarını giriniz: ");
+    double paraMiktari = Convert.ToDouble(Console.ReadLine());
+
+    tekrar:
+    Console.WriteLine("Euro için 'e' - Dolar için 'd' basınız");
+    string birim = Console.ReadLine();
+
+    double sonuc = 0;
+
+    if (birim == "e" || birim == "E")
+    {
+        try
+        {
+            Console.WriteLine("Lütfen Euro kurunu giriniz. ");
+            double euroKur = Convert.ToDouble(Console.ReadLine());
+            sonuc = paraMiktari / euroKur;
+            Console.WriteLine("Euro: " + sonuc);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Geçersiz Euro değeri girdiniz.");
+            goto tekrar;
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Euro değeri sıfır olamaz.");
+            goto tekrar;
+        }
+    }
+    else if (birim == "d" || birim == "D")
+    {
+        try
+        {
+            Console.WriteLine("Lütfen Dolar kurunu giriniz. ");
+            double dolarKur = Convert.ToDouble(Console.ReadLine());
+            sonuc = paraMiktari / dolarKur;
+            Console.WriteLine("Dolar: " + sonuc);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Geçersiz Dolar değeri girdiniz.");
+            goto tekrar;
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Dolar değeri sıfır olamaz.");
+            goto tekrar;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Hatalı giriş. Tekrar deneyin");
+        goto tekrar;
+    }
+}
+catch (FormatException)
+{
+    Console.WriteLine("Geçersiz para miktarı girdiniz.");
+}
