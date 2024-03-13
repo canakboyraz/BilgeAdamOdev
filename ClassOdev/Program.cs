e2f12553e5ac0ad43e8aaf8abb00254ee2cd06b7
canakboyraz <canakboyraz@gmail.com>
2024-03-13 22:49:11 +0300

    Class Odev

---
+++ ClassOdev/Program.cs
@@ -0,0 +1,35 @@
+﻿// Class Odev
+
+using ClassOdev;
+
+Console.WriteLine("Yas giriniz:");
+int.TryParse(Console.ReadLine(), out int yasDegeri);
+
+Takım takım = new Takım();
+
+takım.TakımAdı = "Beşiktaş";
+takım.TakımSayisi = 5;
+takım.Yas = yasDegeri;
+
+try
+{
+    takım.YasKontrol();
+    Console.WriteLine("Yaşın uygun diğer bilgileri girmelisin:");
+    Console.WriteLine("Ad: ");
+    takım.Ad = Console.ReadLine();
+    Console.WriteLine("SoyAd: ");
+    takım.Soyad = Console.ReadLine();
+    
+    Console.WriteLine("Ad: {0}, Soyad: {1}, Yaş: {2}",takım.Ad,takım.Soyad,takım.Yas1);
+}
+catch (Exception e)
+{
+   Console.WriteLine("Hata "+e.Message);
+}
+
+
+//Console.WriteLine("Yas:" + takım.Yas);
+//Console.WriteLine("Yas1:" + takım.Yas1);
+
+
+
---
+++ ClassOdev/Takım.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassOdev
+{
+    internal class Takım
+    {
+        public int TakımSayisi { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int Yas1 { get; set; }
+        public string TakımAdı { get; set; }
+
+        private int _yas;
+
+        public int Yas
+        {
+            get { return _yas; }
+            set { _yas = value; }
+        }
+
+        public void YasKontrol()
+        {
+            if (_yas >= 18)
+            {
+                Yas1 = _yas;
+            }
+            else
+            {
+                throw new Exception("18 Yaşından büyük olmak gerekmektedir." + (18-_yas) + " yas kaldı dayan :)");
+            }
+        }
+
+    }
+
+    
+}
