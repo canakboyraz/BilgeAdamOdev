e82bb7a799b31f6ac171d80e04e20cfcfe0e1076
canakboyraz <canakboyraz@gmail.com>
2024-03-14 22:10:26 +0300

    Params

---
+++ Params/Program.cs
@@ -0,0 +1,42 @@
+﻿// Kullanıcıdan belirsiz sayıda kelime alın ve params olarak metoda gönderin Kullanıcıdan aldığı kelimeleri kullanıcıdan aldığı karakter ile birlşetir.
+string[] kelime = new string[0];
+
+
+
+
+while (true)
+{
+    Console.WriteLine("Çıkmak için 1 - Devam etmek için 2 basınız");
+    int secim = Convert.ToInt32(Console.ReadLine());
+
+    if (secim == 1)
+    {
+        break;
+    }
+
+    try
+    {
+        Console.WriteLine("Kelime giriniz");
+        Array.Resize(ref kelime, kelime.Length + 1);
+        kelime[kelime.Length - 1] = Console.ReadLine();
+    }
+    catch (FormatException ex)
+    {
+        Console.WriteLine("Hatalı giriş yaptınız: " + ex.Message);
+        continue;
+    }
+    catch (OverflowException ex)
+    {
+        Console.WriteLine("Girilen değer sınırların dışında: " + ex.Message);
+        continue;
+    }
+}
+
+Console.WriteLine("Karakter giriniz.");
+char karakter = Convert.ToChar(Console.ReadLine());
+
+Console.WriteLine("Yeni Cümle");
+for (int i = 0; i < kelime.Length; i++)
+{
+    Console.Write(kelime[i] + karakter);
+}
